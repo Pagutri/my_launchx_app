bddec9154997cc4af6812bebdc06430b66ca63cd
Pa Gutri <pagutri@gmail.com>
2022-04-20 09:17:48 -0500

    Create index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const MissionCommander = require('./app/missionCommander');
 const carlo = new MissionCommander("Carlo")
+const paco = new MissionCommander("Paco")
+const luis = new MissionCommander("Luis")
 console.log(carlo.name)
+console.log(paco.name)
+console.log(luis.name)
